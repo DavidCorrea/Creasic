9169860b2db0910e7fc969ba121dfc5fc4aad6be
DavidCorrea <898uu1bb11>
2016-09-10 12:10:54 -0300

    Modificaciones de Ambiente para permitir Angular JS.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,5 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular/angular
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Creasic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.paths << Rails.root.join('vendor','assets','bower_components')
+    config.assets.paths << Rails.root.join('vendor','assets','bower_components','bootstrap-sass-official','assets','fonts')
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
 end
